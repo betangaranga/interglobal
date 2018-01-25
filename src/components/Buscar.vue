5d633c893a67d18bd620595a986bd6f5ac27a558
Angel <albertoangelaguilera@gmail.com>
2018-01-25 02:45:26 -0600

    Se agrega el componente Buscar

---
+++ src/components/Buscar.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <input type="text" v-model="libro"name="" value="">
+</template>
+
+<script>
+export default {
+  name:'buscar',
+  data(){
+    return {
+      libro:''
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
