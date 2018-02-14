d6ab743273988ccb8f7e521925d838bcc9a3bafe
Angel <albertoangelaguilera@gmail.com>
2018-02-13 18:32:33 -0600

    Ya agrega a la base de datos prestamo

--- src/components/Prestamo.vue
+++ src/components/Prestamo.vue
@@ -1,48 +1,76 @@
 <template>
-    <div class="page1">
-      <input type="text" v-model="buscar"class="form-control" id="usr" placeholder="Matricula del Alumno">
-      <div class="libros_prestar">
-        <p>Se realizara el prestamo de :</p>
-        <ul v-for="item in arr">
-          <li>{{item}}
-  </li>
-        </ul>
-      </div>
-      <button type="button" class="btn btn-success" @click="prestar">FInalizar Prestamo</button>
+<div class="page1">
+  <input type="text" v-model="buscar" class="form-control" id="usr" placeholder="Matricula del Alumno">
+  <div class="libros_prestar">
+    <p>Se realizara el prestamo de :</p>
+    <ul v-for="item in arr">
+      <li>{{item}}
+      </li>
+    </ul>
+  </div>
+  <button type="button" class="btn btn-success" @click="prestar">FInalizar Prestamo</button>
 
-    </div>
+</div>
 </template>
 
 <script>
-import { EventBus } from '../event-bus.js';
-var arr=[];
+import '../db'
+import firebase from 'firebase'
+import {
+  EventBus
+} from '../event-bus.js';
+var db = firebase.database();
+var ref_libros = db.ref('libros');
+var ref_alumnos = db.ref('prestamos');
+var arr = [];
+var prestamo=[
+  {}
+];
 EventBus.$on('enviado', radio => {
-  arr=radio;
+  arr = radio;
 });
-    export default {
-        name: 'Prestamo',
-        data () {
-            return {
-                arr: arr
-            }
-        }
+export default {
+  name: 'Prestamo',
+  data() {
+    return {
+      arr: arr,
+      buscar: ""
     }
+  },
+  methods: {
+    prestar: function() {
+      for (var i = 0; i < arr.length; i++) {
+ref_alumnos.push({
+  "fechae":4555,
+  "fechas":6667,
+  "idl":arr[i],
+  "mat":this.buscar,
+  "nomb":"puto"
+})
+      }
+
+    }
+  }
+}
 </script>
 
 <style scoped>
-button{
+button {
   position: relative;
   top: 0px;
 }
-    h1, h2 {
-        font-weight: normal;
-    }
 
-    a {
-        color: #42b983;
-    }
-    ul{
-      position: relative;
-      top: 0px;
-    }
+h1,
+h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+
+ul {
+  position: relative;
+  top: 0px;
+}
 </style>
